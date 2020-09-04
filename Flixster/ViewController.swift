71045be1e0ca33c1a373691f7770c14c7e7cc5e4
Saurabh <Saurabhkaran112gmail.com>
2020-09-03 20:47:30 -0700

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Saurabh Agrawal on 9/3/20.
+//  Copyright © 2020 Codepath Saurabh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
