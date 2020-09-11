5f92b9d740bd94ec6f888a89d34a764c5e557950
Saurabh <Saurabhkaran112gmail.com>
2020-09-10 23:51:41 -0700

    Adding the part of Flixster

--- Flixster/MovieCell.swift
+++ Flixster/MovieCell.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class MovieCell: UITableViewCell {
 
+    @IBOutlet weak var titlelabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Saurabh Agrawal on 9/10/20.
+//  Copyright © 2020 Codepath Saurabh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class MovieGridCell : UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView : UIImageView!
+}
